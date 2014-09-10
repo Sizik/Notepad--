2158e4f64fef1c0211c63530b655db19fa7ef52e
Sizik <the.sizik@gmail.com>
2014-09-10 12:30:15 -0400

    Revert "Revert "Revert "Revert "Initial commit""""
    This reverts commit edbd60d14335076057101abe81ead2f528835862.

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Notepad__
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Form1(String file)
+        {
+            InitializeComponent();
+            string lines = System.IO.File.ReadAllText(file);
+            this.textBox1.Text = lines;
+            this.textBox1.ReadOnly = false;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
